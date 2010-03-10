bf5aaba8e0469783bcbcaa8055ac8895f959574d
Bart J Nagel <bjn@ecs.soton.ac.uk>
2010-03-10 15:06:51 +0000

    merged with upstream

--- include/constants.php
+++ include/constants.php
@@ -13,7 +13,7 @@ licence -- see the LICENCE file for more details
 
 // program
 define("PROGRAMNAME", "Eqiat");
-define("VERSION", "0.3~bpc");
+define("VERSION", "0.3~git~bpc");
 
 // filesystem path to the eqiat root directory -- one level above this file, 
 // ending in a trailing slash
